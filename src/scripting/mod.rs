90fa76f9137dd5ec16c9bbfabe6a0ea3f19303cd
Jason Lee <lwlee2608@gmail.com>
2024-09-09 11:46:32 +0800

    Support substring and lastIndexOf function in Script::parse_line()

--- src/scripting/mod.rs
+++ src/scripting/mod.rs
@@ -2,6 +2,7 @@
 #![allow(dead_code)]
 use crate::function;
 use crate::script::{Script, ScriptVariable};
+use regex::Regex;
 
 // Experimental
 
@@ -32,30 +33,71 @@ impl Parser {
             return Err("invalid script, expected '='".into());
         }
 
+        let mut args = vec![];
+
         // Crude way to determine function
         // * doesn't work with space char in string
+        // This part need refactoring
+        let function = match parts.len() {
+            4 => {
+                let rhs = parts[3];
+                // Check if '.' is present
+                // Example: location.substring(location.lastIndexOf('/'))
+                // location will be the variable, substring will be the function
+                let parts: Vec<&str> = rhs.split('.').collect();
+                if parts.len() == 2 {
+                    if parts[1].contains('(') {
+                        // function with arguments
+                        // extract functions arguments using regex
+                        let re = Regex::new(r"(\w+)\((.*)\)").unwrap();
+                        let caps = re.captures(parts[1]).unwrap();
+                        let func_name = caps.get(1).unwrap().as_str();
+                        let func_args = caps.get(2).unwrap().as_str();
 
-        let mut args = vec![];
-        let function = if parts.len() == 4 {
-            let arg0 = ScriptVariable::from_str(parts[3]);
-            args.push(arg0);
+                        // TODO recursive function make more sense
+                        if func_name == "substring" {
+                            let arg0 = ScriptVariable::from_str(parts[0]);
+                            let arg1 = ScriptVariable::from_str(func_args);
+                            args.push(arg0);
+                            args.push(arg1);
 
-            function::Function::Copy(function::CopyFunction {})
-        } else if parts.len() == 5 {
-            return Err("invalid script, expected function".into());
-        } else if parts.len() == 6 {
-            let operator = parts[4];
-            if operator != "+" {
-                return Err("invalid script, only '+' operator is supported".into());
+                            function::Function::SubString(function::SubStringFunction {})
+                        } else if func_name == "lastIndexOf" {
+                            let arg0 = ScriptVariable::from_str(parts[0]);
+                            let arg1 = ScriptVariable::from_str(func_args);
+                            args.push(arg0);
+                            args.push(arg1);
+
+                            function::Function::LastIndexOf(function::LastIndexOfFunction {})
+                        } else {
+                            return Err("invalid script, expected function".into());
+                        }
+                    } else {
+                        return Err("invalid script, expected function with arguments".into());
+                    }
+                } else {
+                    // else it's a simple assignment
+                    let arg0 = ScriptVariable::from_str(rhs);
+                    args.push(arg0);
+
+                    function::Function::Copy(function::CopyFunction {})
+                }
             }
-            let arg0 = ScriptVariable::from_str(parts[3]);
-            let arg1 = ScriptVariable::from_str(parts[5]);
-            args.push(arg0);
-            args.push(arg1);
+            6 => {
+                let operator = parts[4];
+                if operator != "+" {
+                    return Err("invalid script, only '+' operator is supported".into());
+                }
+                let arg0 = ScriptVariable::from_str(parts[3]);
+                let arg1 = ScriptVariable::from_str(parts[5]);
+                args.push(arg0);
+                args.push(arg1);
 
-            function::Function::Plus(function::PlusFunction {})
-        } else {
-            todo!()
+                function::Function::Plus(function::PlusFunction {})
+            }
+            _ => {
+                return Err("invalid script, expected function".into());
+            }
         };
 
         let return_var_name = parts[1].to_string();
@@ -108,7 +150,7 @@ mod tests {
     }
 
     #[test]
-    fn test_scripting_split() {
+    fn test_scripting_extract_location() {
         let global = Global::empty();
         let global = Arc::new(RwLock::new(global));
         let mut context = ScriptContext::new(global);
@@ -125,12 +167,21 @@ mod tests {
         // def chargingDataRef = location.substring(location.lastIndexOf('/') + 1)
         //
         // def index = location.lastIndexOf('/')
-        // let script = Parser::parse_line("def index = location.lastIndexOf('/')").unwrap();
+        let script = Parser::parse_line("def index = location.lastIndexOf('/')").unwrap();
+        script.execute(&mut context).unwrap();
+        assert_eq!(context.get_variable("index").unwrap().as_int(), 34,);
 
         // def index = index + 1
+        let script = Parser::parse_line("def index = index + 1").unwrap();
+        script.execute(&mut context).unwrap();
+        assert_eq!(context.get_variable("index").unwrap().as_int(), 35,);
+
         // def chargingDataRef = location.substring(index)
-        //
-        //
-        // TODO
+        let script = Parser::parse_line("def chargingDataRef = location.substring(index)").unwrap();
+        script.execute(&mut context).unwrap();
+        assert_eq!(
+            context.get_variable("chargingDataRef").unwrap().as_string(),
+            "123"
+        );
     }
 }
